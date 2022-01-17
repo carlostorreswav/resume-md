9c7a5bf272153073a4ad8564fb2aa3759da9261f
Carlos Torres <carlos.torres@secuoyas.com>
2022-01-17 23:58:34 +0100

    auto

---
+++ src/FirebaseContext.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export const FirebaseContext = React.createContext({});
+
+export const FirebaseProvider = ({ children }) => {
+    const [ctx, setCtx] = useState("hello")
+  return (
+    <FirebaseContext.Provider value={{ctx, setCtx})>
+      {children}
+    </FirebaseContext.Provider>
+  );
+};
