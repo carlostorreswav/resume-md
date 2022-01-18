f377ba4f5776522d9e514a0d2355314ada947fd5
Carlos Torres <carlos.torres@secuoyas.com>
2022-01-18 23:49:07 +0100

    auto

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { AppProvider } from "./AppContext"
 
 const GlobalStyle = createGlobalStyle`
   body {
+    scroll-behavior: smooth;
     font-family: ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol";
     margin: 0;
   }
