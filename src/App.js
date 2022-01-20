b2021bd0bf5c94e768ec12f0f41cb8012f3577ae
Carlos Torres <carlos.torres@secuoyas.com>
2022-01-20 09:56:59 +0100

    lazy

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
-import Page from "./Page";
-import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+// import Page from "./Page"
+// import Home from "./Home"
+import { Suspense, lazy } from "react"
+
+const Home = lazy(() => import("./Home"))
+const Page = lazy(() => import("./Page"))
 
 const App = () => {
   return (
-  <Router>
-    <Routes>
-      <Route exact path="/" element={<Home/>}/>
-      <Route exact path="/:id" element={<Page/>}/>
-    </Routes>
-  </Router>
+    <Router>
+      <Routes>
+        <Suspense fallback={<></>}>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/:id" element={<Page />} />
+        </Suspense>
+      </Routes>
+    </Router>
   )
 }
 
